194e449cfe9189e2d655ec92a7e07d7bef21effb
Alex Sobolev <alexandr.sobolev@deliveryhero.com>
2021-04-07 22:09:08 +0200

    Add style to Todo component

--- src/components/Todo/TodoHeader/TodoHeader.tsx
+++ src/components/Todo/TodoHeader/TodoHeader.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useObserver } from 'mobx-react';
 import { useRootStore } from '../../../stores';
+import './TodoHeader.css';
 
 export const TodoHeader = () => {
   const { todoStore } = useRootStore();
 
-  return useObserver(() => <header className="App-header">{todoStore.todosCount} Todos:</header>);
+  return useObserver(() => (
+    <header className="TodoHeader">
+      <span className="TodoHeader-count">{todoStore.todosCount}</span> Todos:
+    </header>
+  ));
 };
